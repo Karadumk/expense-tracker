3e7a55387cb546222a69778556656c5c718f6ac7
Karadumk <karadumk223@gmail.com>
2025-05-10 14:40:40 +0100

    feat(main): initialize FastAPI app with user routes

--- app/main.py
+++ app/main.py
@@ -4,10 +4,17 @@ from app.database import get_db
 from app import models
 from sqlalchemy.orm import Session
 from app.routers import users
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(users.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/test-db")
