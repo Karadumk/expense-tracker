963e76d34618cb00032dd24d15357a1a65e9a5f2
Karadumk <karadumk223@gmail.com>
2025-05-10 14:39:32 +0100

    feat(crud): refactor user creation and add password authentication logic

--- app/crud.py
+++ app/crud.py
@@ -11,6 +11,7 @@ def get_user_by_email(db: Session, email: str):
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = auth.hash_password(user.password)
     db_user = models.User(
+        username=user.username,
         email=user.email,
         hashed_password=hashed_password
     )
@@ -18,3 +19,12 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
+    if not user:
+        return False
+    if not auth.verify_password(password, user.hashed_password):
+        return False
+    return user
